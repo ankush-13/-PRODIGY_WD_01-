21bf04a98df433b95233a3335df5b13badb45913
ankush-13 <ankushpatle321@gmail.com>
2024-12-03 16:33:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.getElementById('navbar');
+  
+    // Add a class to the navbar when scrolling
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
+  
+  });
